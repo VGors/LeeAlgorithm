8aeb2af6ed06ae009c7c3f9434ef2bac92337aa8
Victor Gorshenev <victorgorshenev@MacBook-Air-Victor.local>
2021-01-24 21:58:51 +0300

    Lee algorithm added

---
+++ src/test/java/LeeAlgorithm.java
@@ -0,0 +1,22 @@
+public class LeeAlgorithm {
+    private static final int FIELD_SIZE_X = 10;
+    private static final int FIELD_SIZE_Y = 10;
+    private static int[][] wayField;
+
+    public static void main(String[] args) {
+        initialization();
+    }
+
+    private static void initialization(){
+        int obstacles = (int) (FIELD_SIZE_X * FIELD_SIZE_Y * 0.35);
+
+    }
+
+    private static void waveSpreading(){
+
+    }
+
+    private static void restoreWay(){
+
+    }
+}
